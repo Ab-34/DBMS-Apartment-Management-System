3e8c2eb07fb0184a43fa5d7d1a5a03408dc68625
Abhijnya Bhat <55902724+Ab-34@users.noreply.github.com>
2021-11-16 12:33:02 +0530

    Update x.py

--- x.py
+++ x.py
@@ -3,58 +3,81 @@ import pandas as pd
 import numpy as np
 import pickle as pkle
 import os.path
-
+import psycopg2
 
 st.title('Apartment Management System')
 
+conn = psycopg2.connect(host='localhost',
+                      port='5432',
+                      database='t_15_57_59',
+                      user= 'postgres',
+                      password= 'dbms')
+
+conn.autocommit = True
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
 
-# # create a button in the side bar that will move to the next page/radio button choice
-# next = st.sidebar.button('Go To')
 
-# # will use this list and next button to increment page, MUST BE in the SAME order
-# # as the list passed to the radio button
-# new_choice = ['Home','Facilities','Log In','Rent','Maintenance']
+# create a button in the side bar that will move to the next page/radio button choice
+next = st.sidebar.button('Go To')
 
-# # This is what makes this work, check directory for a pickled file that contains
-# # the index of the page you want displayed, if it exists, then you pick up where the
-# #previous run through of your Streamlit Script left off,
-# # if it's the first go it's just set to 0
-# if os.path.isfile('next.p'):
-#     next_clicked = pkle.load(open('next.p', 'rb'))
-#     # check if you are at the end of the list of pages
-#     if next_clicked == len(new_choice):
-#         next_clicked = 0 # go back to the beginning i.e. homepage
-# else:
-#     next_clicked = 0 #the start
+# will use this list and next button to increment page, MUST BE in the SAME order
+# as the list passed to the radio button
+new_choice = ['Home','Facilities','Log In','Rent','Maintenance']
 
-# # this is the second tricky bit, check to see if the person has clicked the
-# # next button and increment our index tracker (next_clicked)
-# if next:
-#     #increment value to get to the next page
-#     next_clicked = next_clicked +1
+# This is what makes this work, check directory for a pickled file that contains
+# the index of the page you want displayed, if it exists, then you pick up where the
+#previous run through of your Streamlit Script left off,
+# if it's the first go it's just set to 0
+if os.path.isfile('next.p'):
+    next_clicked = pkle.load(open('next.p', 'rb'))
+    # check if you are at the end of the list of pages
+    if next_clicked == len(new_choice):
+        next_clicked = 0 # go back to the beginning i.e. homepage
+else:
+    next_clicked = 0 #the start
 
-#     # check if you are at the end of the list of pages again
-#     if next_clicked == len(new_choice):
-#         next_clicked = 0 # go back to the beginning i.e. homepage
+# this is the second tricky bit, check to see if the person has clicked the
+# next button and increment our index tracker (next_clicked)
+if next:
+    #increment value to get to the next page
+    next_clicked = next_clicked +1
 
-# # create your radio button with the index that we loaded
-# choice = st.sidebar.radio("go to",('Home','Facilities','Log In','Rent','Maintenance'), index=next_clicked)
+    # check if you are at the end of the list of pages again
+    if next_clicked == len(new_choice):
+        next_clicked = 0 # go back to the beginning i.e. homepage
 
-# # pickle the index associated with the value, to keep track if the radio button has been used
-# pkle.dump(new_choice.index(choice), open('next.p', 'wb'))
+# create your radio button with the index that we loaded
+choice = st.sidebar.radio("go to",('Home','Facilities','Log In','Rent','Maintenance'), index=next_clicked)
 
-# # finally get to whats on each page
-# if choice == 'Home':
-#     st.write('this is home')
-# elif choice == 'Facilities':
-#     st.write('here is a Facilities page')
-# elif choice == 'Log In':
-#     st.write('A Log In of some sort')
-# elif choice == 'Rent':
-#     st.write('The Rent')
-# elif choice == 'Maintenance':
-#     st.write('Maintenance page')
+# pickle the index associated with the value, to keep track if the radio button has been used
+pkle.dump(new_choice.index(choice), open('next.p', 'wb'))
+
+# finally get to whats on each page
+if choice == 'Home':
+    st.write('this is home')
+elif choice == 'Facilities':
+    st.write('here is a Facilities page')
+    rows = run_query("SELECT * from facilities;")
+    indoor=[row[1] for row in rows if row[2]=="Indoor"]
+    outdoor=[row[1] for row in rows if row[2]=="Outdoor"]
+
+    # Print results.
+    st.write("Indoor")
+    st.write(indoor)
+    st.write("Outdoor")
+    st.write(outdoor)
+    # for row in rows:
+    #     st.write(f"{row[0]}.{row[1]}")
+elif choice == 'Log In':
+    st.write('A Log In of some sort')
+elif choice == 'Rent':
+    st.write('The Rent')
+elif choice == 'Maintenance':
+    st.write('Maintenance page')
 
 # def fun():
 #     st.write('fun click')
@@ -82,27 +105,28 @@ st.title('Apartment Management System')
 #     st.write('Goodbye')
 
 
-import psycopg2
 
+# sql = """SELECT * FROM owner """
+# # sql = """INSERT INTO main_fkl_mini_2 (project_name ,sponsor1 ,sponsor2 ,sponsorsh1 ,sponsorsh2 ) VALUES (%s,%s,%s,%s,%s)"""
 
-# Initialize connection.
-# Uses st.cache to only run once.
-@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
-def init_connection():
-    return psycopg2.connect(**st.secrets["postgres"])
+# cursor = conn.cursor()
 
-conn = init_connection()
+# st.write(cursor.execute(sql))
 
-# Perform query.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+# # Initialize connection.
+# # Uses st.cache to only run once.
+# @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+# def init_connection():
+#     return psycopg2.connect(**st.secrets["postgres"])
 
-rows = run_query("SELECT * from owner;")
+# conn = init_connection()
 
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+# # Perform query.
+# # Uses st.cache to only rerun when the query changes or after 10 min.
+# @st.cache(ttl=600)
+
+# x = run_query("SELECT * from owner;")
+
+# # Print results.
+# for row in x:
+#     st.write(f"{x[0]} has a :{x[1]}:")
